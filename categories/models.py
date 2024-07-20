4ec67bc079b964ae13d5a3b1ab5d65b108463810
Tahdi Islam <tshdi60@gmail.com>
2024-07-20 16:48:30 +0600

    add image field in category

--- categories/models.py
+++ categories/models.py
@@ -4,6 +4,7 @@ from django.db import models
 class Category(models.Model):
     name = models.CharField(max_length=50, blank=False)
     slug = models.SlugField(max_length=60, unique=True)
+    image = models.ImageField(upload_to='categories/images/', null=True, blank=True)
 
     def __str__(self):
         return self.name
