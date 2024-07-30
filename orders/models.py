6c8031b3553f4da2922f7986be08a047b02ab28d
Tahdi Islam <tshdi60@gmail.com>
2024-07-30 22:58:08 +0600

    add some payment related fiels in order mode

--- orders/models.py
+++ orders/models.py
@@ -15,6 +15,8 @@ class Order(models.Model):
     quantity = models.IntegerField()
     total_price = models.FloatField()
     created_at = models.DateTimeField(auto_now_add=True)
+    ordered = models.BooleanField(default=False, blank=True)
+    transaction_id = models.CharField(max_length=100, null=True, blank=True)
 
     def __str__(self):
         return f"{self.customer} {self.flower} {self.quantity} {self.total_price}"
